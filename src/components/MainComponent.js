e12ef5198f12b08122f1a80ab7be59c85175f63b
muuhab <mohab.barca@gmail.com>
2021-01-24 19:49:19 +0200

    SPA part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,21 +4,32 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments:COMMENTS,
+      leaders:LEADERS,
+      promotions:PROMOTIONS
     };
   }
  
   render() {
     const HomePage=()=>{
         return(
-           <Home/>
+          <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />
         );
     }
     return (
@@ -27,6 +38,7 @@ class Main extends Component {
       <Switch>
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+          <Route exact path="/contact-us" component={Contact}/>
           <Redirect to="/home"/>
       </Switch>
       <Footer/>
